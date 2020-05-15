d75d4546b93b22e6bb1aafab0fe516b6611a3277
Lohitha Yerramsetty <lohitha.yerramsetty@gmail.com>
2020-05-15 17:23:47 +0200

    Even Numbers

---
+++ Week1_Evennumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Evennumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumbers = new int[10];
+
+            Console.WriteLine("Enter the 10 integer Numbers");
+
+            for (int i = 0; i < myNumbers.Length; i++)
+            {
+
+                myNumbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("List of even Numbers:");
+
+            for (int i = 0; i < myNumbers.Length; i++)
+            {
+                if (myNumbers[i] % 2 == 0) 
+                    Console.WriteLine(myNumbers [i] );
+
+            }
+        }              
+    }
+}
+
